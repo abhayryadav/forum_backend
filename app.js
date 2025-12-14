f0abb787a0dea437a10443a790a3c681c67fc83b
Abhay Raj Yadav <abhayryadav0007@gmail.com>
2025-12-14 18:34:22 +0530

    5

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -13,13 +13,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-// Middleware to attach user to req for superuser check
-app.use((req, res, next) => {
-  if (req.session.userId) {
-    // Simplified; fetch role in routes
-  }
-  next();
-});
+
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/task'));
